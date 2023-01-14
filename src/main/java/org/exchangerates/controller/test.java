ec7ce44f8ace67dcb4039976c70834f260200859
Mariusz Plaskota <mariusz.plaskota@o2.pl>
2023-01-14 18:16:16 +0100

    Done.

---
+++ src/main/java/org/exchangerates/controller/test.java
@@ -0,0 +1,20 @@
+package org.exchangerates.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class test {
+
+    @GetMapping("/now")
+    @ResponseBody
+    public String testing()
+    {
+        return "Strona testowa";
+    }
+}
