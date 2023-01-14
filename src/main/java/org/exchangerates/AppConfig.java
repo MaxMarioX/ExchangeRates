f25a6885a5af6bbaeba3af1a772b285aacc41f59
Mariusz Plaskota <mariusz.plaskota@o2.pl>
2023-01-14 19:38:47 +0100

    Done.

--- src/main/java/org/exchangerates/AppConfig.java
+++ src/main/java/org/exchangerates/AppConfig.java
@@ -1,23 +1,25 @@
 package org.exchangerates;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan
 public class AppConfig implements WebMvcConfigurer {
 
-    public AppConfig()
+    @Bean
+    public ViewResolver viewResolver()
     {
-
-    }
-    @Override
-    public void configureViewResolvers(ViewResolverRegistry registry)
-    {
-        registry.jsp("/WEB-INF/views/",".jsp");
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/views/");
+        viewResolver.setSuffix(".jsp");
+        return viewResolver;
     }
 }
